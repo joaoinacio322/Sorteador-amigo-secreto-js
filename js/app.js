04c3a8542f0bef4d418ab7802d9e3acd8b3c748a
Joao Inacio <joaoinacio322@gmail.com>
2025-06-27 20:56:57 -0300

    Adicionando função reiniciar

--- js/app.js
+++ js/app.js
@@ -35,3 +35,12 @@ function embaralhar(lista) {
         [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
+    
+
+
